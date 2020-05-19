3d74d5cb0e1cd27027361f26229c9ed63c9c76a7
Willy Romão <willyr.goncalves@gmail.com>
2020-05-19 01:39:52 +0000

    improve the graph and save on ./downloads/

--- show_macro_brazil.py
+++ show_macro_brazil.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 """This is script show brazil macro economics graphs."""
 
+import time
+
 import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -10,6 +12,8 @@ import yfinance as yf
 sns.set()
 
 matplotlib.rcParams["figure.figsize"] = (16, 8)
+matplotlib.rcParams.update({"figure.autolayout": True})
+timestr = time.strftime("%Y%m%d_%H%M%S")
 
 
 def get_bc_data(cod_bc):
@@ -45,7 +49,31 @@ ibov_diff_agg.iloc[0] = 1
 
 cdi_agg = (1 + cdi[cdi.index >= start_date] / 100).cumprod()
 cdi_agg.iloc[0] = 1
+selic_meta_date = selic_meta[selic_meta.index >= start_date]
+dollar_reserve_date = dollar_reserve[dollar_reserve.index >= start_date]
+dollar_reserve_date_bi = dollar_reserve_date["valor"].div(1000000)
+
+print(ibov_diff_agg)
+print(cdi_agg)
+print(selic_meta_date)
+print(dollar_reserve_date_bi)
 
 fig, ax = plt.subplots()
-ax.plot(ibov_diff_agg)
-ax.plot(cdi_agg)
+ax.set_title("show_macro_brazil.py")
+
+ax.xaxis.set_label_text("Date")
+ax.yaxis.set_label_text("Values")
+
+ax.plot(ibov_diff_agg, label="ibov_return_agg")
+ax.plot(cdi_agg, label="cdi_return_agg")
+ax.plot(selic_meta_date, label="selic_meta")
+ax.plot(dollar_reserve_date_bi, label="dollar_reserve_in_tri")
+
+ax.legend([
+    "ibov_return_agg",
+    "cdi_return_agg",
+    "selic_meta",
+    "dollar_reserve_in_tri"
+ ])
+
+fig.savefig("./downloads/show_macro_brazil_plot_" + timestr + ".png")
